175dba24a57d5b17fcaef4ea2982a5dad5ed7da8
yeshrajawat <yeshrajawat@gmail.com>
2022-05-30 13:20:33 +0530

    Initialized git with MVC structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
